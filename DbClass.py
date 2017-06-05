4c24a6a842b1236dbb355ee56ea2fdc36e7f142c
MathieuBonteHowest <mathieu.bonte@student.howest.be>
2017-06-05 15:35:44 +0200

    Initial commit

---
+++ DbClass.py
@@ -0,0 +1,55 @@
+class DbClass:
+    def __init__(self):
+        import mysql.connector as connector
+
+        self.__dsn = {
+            "host": "localhost",
+            "user": "root",
+            "passwd": "mb25081998",
+            "db": "db_lightz"
+        }
+
+        self.__connection = connector.connect(**self.__dsn)
+        self.__cursor = self.__connection.cursor()
+
+
+    def getUser(self, username):
+        # Query met parameters
+        sqlQuery = "SELECT password FROM tbl_users WHERE login = '{param1}'"
+        # Combineren van de query en parameter
+        sqlCommand = sqlQuery.format(param1=username)
+
+        self.__cursor.execute(sqlCommand)
+        result = self.__cursor.fetchone()
+        self.__cursor.close()
+        return result
+
+    def getDataFromDatabase(self):
+        # Query zonder parameters
+        sqlQuery = "SELECT * FROM tablename"
+
+        self.__cursor.execute(sqlQuery)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def getDataFromDatabaseMetVoorwaarde(self, voorwaarde):
+        # Query met parameters
+        sqlQuery = "SELECT * FROM tablename WHERE columnname = '{param1}'"
+        # Combineren van de query en parameter
+        sqlCommand = sqlQuery.format(param1=voorwaarde)
+
+        self.__cursor.execute(sqlCommand)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def setDataToDatabase(self, value1):
+        # Query met parameters
+        sqlQuery = "INSERT INTO tablename (columnname) VALUES ('{param1}')"
+        # Combineren van de query en parameter
+        sqlCommand = sqlQuery.format(param1=value1)
+
+        self.__cursor.execute(sqlCommand)
+        self.__connection.commit()
+        self.__cursor.close()
---
+++ LightzProject.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
+from DbClass import DbClass
+
+app = Flask(__name__)
+
+app.secret_key = "MHkGBcKLloYeSp4FDaCsftFTesfDea7w"
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+
+    if request.method == 'POST':
+        db = DbClass()
+
+        pwd = db.getUser(request.form['username'])
+
+        if request.form['password'] != pwd[0]:
+            error = "Invalid credentials. Please try again."
+        else:
+            session['logged_in'] = True
+            flash('You were just logged in!')
+            return redirect(url_for('controlpanel'))
+    return render_template("login.html", error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out!')
+    return redirect(url_for('login'))
+
+@app.route('/controlpanel')
+@login_required
+def controlpanel():
+    return render_template('controlpanel.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
