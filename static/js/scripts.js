4f48431504b6d2caf23c3aa9c8431e2218dba801
MathieuBonteHowest <mathieu.bonte@student.howest.be>
2017-06-07 17:34:32 +0200

    overzicht page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,16 +1,23 @@
 /**
  * Created by Mathi_000 on 6/06/2017.
  */
+var delayMillis = 500; //1 second
 
-$(document).ready(function()
+setTimeout(function() {
+  //your code to be executed after 1 second
+    $(document).ready(function()
 {
     if (typeof window.sessionStorage != undefined){
         if (!sessionStorage.getItem('mySessionVal')){
+
             document.getElementById('flash').style.display = "block";
-            $("#flash").fadeIn("slow").delay(2500).fadeOut("slow");
+            $("#flash").fadeIn("slow").delay(2000).fadeOut("slow");
             sessionStorage.setItem('mySessionVal', true);
             sessionStorage.setItem('storedWhen', Date.now());
         }
     }
 });
+}, delayMillis);
+
+
 
